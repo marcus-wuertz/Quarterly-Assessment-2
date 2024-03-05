4f2e1afa5de8dcc003c2667e14e04d59488ecbd1
Marcus Wuertz <160975640+marcus-wuertz@users.noreply.github.com>
2024-03-05 17:15:40 -0600

    Created table to store values for Business Analytics Capstone

--- quiz_bowl_db.py
+++ quiz_bowl_db.py
@@ -38,3 +38,14 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS ProgrammingLogic (
                     option4 TEXT,
                     correct_answer TEXT
                 )''')
+
+# Define SQL command to create a table to hold questions for Business Intelligence and Analytics Capstone
+cursor.execute('''CREATE TABLE IF NOT EXISTS AnalyticsCapstone (
+                    id INTEGER PRIMARY KEY,
+                    question TEXT,
+                    option1 TEXT,
+                    option2 TEXT,
+                    option3 TEXT,
+                    option4 TEXT,
+                    correct_answer TEXT
+                )''')
